f5e776a2a0f91b8f6249593339e60f4b7d0c55ae
Eduardo de Souza <sephirothff71@gmail.com>
2024-10-26 15:43:57 -0300

    feat: html, data and css adjusted. Final version

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@ import data from '@/data/cardapio.json';
 
 <template>
   <div class="container py-4">
-    <div class="mx-auto flex flex-col gap-y-10 max-w-screen-sm">
-
-      <div class="bg-app-red pt-3 pb-2">
-        <h1 class="text-center text-white text-4xl font-bold font-bebasNeue">Cardápio 31/10/2024</h1>
+    <div class="mx-auto flex max-w-screen-sm flex-col gap-y-10">
+      <div class="bg-app-red pb-2 pt-3">
+        <h1 class="text-center font-bebasNeue text-4xl font-bold text-white">
+          Cardápio 31/10/2024
+        </h1>
       </div>
 
       <div
@@ -15,23 +16,36 @@ import data from '@/data/cardapio.json';
         :key="items.title"
         class="flex flex-col gap-y-6"
       >
-        <h2 class="text-center text-app-red font-bold  font-bebasNeue text-4xl">{{ items.title }}</h2>
+        <h2 class="text-center font-bebasNeue text-4xl font-bold text-app-red">
+          {{ items.title }}
+        </h2>
         <div
           v-for="item in items.item"
           :key="item.title"
-          class="flex gap-x-4 items-center"
+          class="flex items-center gap-x-6 border-b pb-6"
         >
           <img
             :src="item.image"
-            class="object-contain h-32 w-32"
-          >
+            class="h-32 w-32 object-contain"
+          />
 
-          <div class="flex flex-col gap-y-3">
-            <h3 class="font-bold">
+          <div class="flex flex-col gap-y-2">
+            <h3 class="font-bebasNeue text-app-darkRed text-xl leading-6">
               {{ item.title }}
             </h3>
-            <span>{{ item.content }}</span>
-            <span>{{ item.price }}</span>
+            <span
+              v-if="item.subtitle"
+              class="text-sm"
+            >
+              {{ item.subtitle }}
+            </span>
+
+            <span
+              v-if="item.content"
+              class="text-sm"
+            >
+              Quantidade: {{ item?.content }}</span>
+            <span class="font-bold text-sm">{{ item.price }}</span>
           </div>
         </div>
       </div>
