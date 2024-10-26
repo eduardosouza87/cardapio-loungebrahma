dcb9c84c9ad65f6a20a61ea6104ee062af99520a
Eduardo de Souza <sephirothff71@gmail.com>
2024-10-26 14:52:29 -0300

    feat: json finished

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,83 @@
-import js from '@eslint/js'
+import pluginJs from '@eslint/js'
+import pluginTsParser from '@typescript-eslint/parser'
+import prettierConfig from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import globals from 'globals'
+import pluginTs from 'typescript-eslint'
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
+  pluginJs.configs.recommended,
+  ...pluginTs.configs.recommended,
+  ...pluginTs.configs.strict,
+  ...pluginTs.configs.stylistic,
   ...pluginVue.configs['flat/essential'],
-  skipFormatting,
+
+  {
+    ignores: ['node_modules/**', 'dist', 'eslint.config.js'],
+
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: pluginTsParser,
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        parser: 'vue-eslint-parser',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+
+    plugins: {
+      prettier,
+      '@typescript-eslint/parser': pluginTsParser,
+    },
+
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      quotes: ['warn', 'single'],
+      'no-empty': ['warn', { allowEmptyCatch: true }],
+      '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+      curly: ['error', 'all'],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'id-length': ['error', { min: 2, exceptions: ['_'] }],
+    },
+  },
+
+  {
+    files: ['**/*.vue'],
+
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        parser: pluginTsParser,
+      },
+    },
+
+    plugins: {
+      vue: pluginVue,
+      prettier,
+    },
+
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      'vue/require-valid-default-prop': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-deprecated-slot-attribute': 'off',
+      'vue/no-multiple-template-root': ['error'],
+      'vue/no-unused-vars': 'error',
+      'vue/component-tags-order': [
+        'error',
+        {
+          order: ['script', 'template', 'style'],
+        },
+      ],
+      'vue/component-api-style': ['error', ['script-setup', 'composition']],
+    },
+  },
 ]
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import data from '@/data/cardapio.json';
 </script>
 
 <template>
-  <div></div>
+  <div class="container py-4">
+    <div class="mx-auto flex flex-col gap-y-10 max-w-screen-sm">
+
+      <div class="bg-app-red pt-3 pb-2">
+        <h1 class="text-center text-white text-4xl font-bold font-bebasNeue">Cardápio 31/10/2024</h1>
+      </div>
+
+      <div
+        v-for="items in data"
+        :key="items.title"
+        class="flex flex-col gap-y-6"
+      >
+        <h2 class="text-center text-app-red font-bold  font-bebasNeue text-4xl">{{ items.title }}</h2>
+        <div
+          v-for="item in items.item"
+          :key="item.title"
+          class="flex gap-x-4 items-center"
+        >
+          <img
+            :src="item.image"
+            class="object-contain h-32 w-32"
+          >
+
+          <div class="flex flex-col gap-y-3">
+            <h3 class="font-bold">
+              {{ item.title }}
+            </h3>
+            <span>{{ item.content }}</span>
+            <span>{{ item.price }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="postcss" scoped>
-
-</style>
+<style lang="postcss" scoped></style>
